e721171a7666c12ec2b2d085a1833b66d3e3d4b6
basybe <basybemoog@gmail.com>
2024-08-20 14:21:49 +0500

    some update yaml file, create config.go file

---
+++ internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env-required:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"./data"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GrpcConfig    `yaml:"grpc"`
+}
+
+type GrpcConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := FetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config path does not exist" + path)
+	}
+	var config Config
+	if err := cleanenv.ReadConfig(path, &config); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+	return &config
+}
+
+func FetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "./config.yaml", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+	return res
+}
